ef861acdb769be668697c1811376e2bdca341588
motm <jd@motm.de>
2013-07-08 19:59:55 +0200

    read from index.html

--- web.js
+++ web.js
@@ -1,10 +1,29 @@
 var express = require('express');
+var fs = require('fs');
+var buf = require('buffer')
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  
+  readFromFile("./index.html", response);
+  //response.send('Hello World2!');
 });
+
+
+var readFromFile = function(fileName, response) {
+	fs.readFile(fileName, function (err, data) {
+		if (err) throw err;
+  		writeBuff(data, response)
+	});
+};
+
+
+var writeBuff = function(data, response){
+	var buf = new Buffer(data);
+	//console.log(buf.toString());
+	response.send(buf.toString());
+}
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
